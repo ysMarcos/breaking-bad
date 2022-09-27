173533b6ec1405918c9dda5f8a71302fe716ec2f
marguilherme <84818691+marguilherme@users.noreply.github.com>
2022-09-27 15:59:17 -0300

    correção problema de rota

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
-<?php
-    include 'config.php';
-?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -9,9 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quebrando ruim API</title>
     <!-- local host do marcos -->
-    <!-- <base href="http://localhost/breaking-bad/"> -->
+    <base href="http://localhost/breaking-bad/">
     <!-- local host do kevin -->
-    <base href="http://localhost:81/faculdade/breaking-bad/breaking-bad/">    
+    <!--<base href="http://localhost:81/faculdade/breaking-bad/breaking-bad/">  -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="./css/style.css">
@@ -26,7 +24,7 @@
         <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
             <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+            <a class="nav-link active" aria-current="page" href="home">Home</a>
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -48,21 +46,21 @@
             </ul>
             </li>
             <li class="nav-item">  
-                <a class="nav-link active" href="morte">Mortes</a>
+                <a class="nav-link" href="morte">Mortes</a>
             </li>
         </ul>
         </div>
     </div>
     </nav>
 
-    <main>
+    <main class="container">
         <?php
             $pagina = "home";
 
             if(isset($_GET["param"])){
                 $param = trim($_GET["param"]);
                 $param = explode("/", $param);
-
+                print_r($param);
                 $pagina = $param[0];
             }
 
--- paginas/morte.php
+++ paginas/morte.php
@@ -1,17 +1,12 @@
 <?php
-    include '../config.php';
-?>
+    $url = "https://www.breakingbadapi.com/api/random-death?limit=1&offset=1";
+
+    $dados = file_get_contents($url);
+    $dados = json_decode($dados);
+    foreach($dados as $morte){
+        ?>   
+            <p><strong><?= $morte->death ?></strong></p>
         <?php
-        $url = "https://www.breakingbadapi.com/api/random-death";
-
-        $dados = file_get_contents($url);
-        $dados = json_decode($dados);
-        foreach($dados as $personagem){
-
-            ?>   
-                <p><strong><?= $personagem ?></strong></p>
-            <?php
-        }
-   
+    }
 ?>
 
