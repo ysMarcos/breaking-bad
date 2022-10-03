9d4b9c06c5c94c33415740f61d02b42d56bd15f4
NivekSotam <kevin.matoss.km@gmail.com>
2022-10-03 20:33:12 -0300

    ajustes2

--- paginas/morte.php
+++ paginas/morte.php
@@ -1,15 +1,17 @@
 <?php
-
-    $urlMorte = "https://www.breakingbadapi.com/api/random-death?limit=1&offset=1";
+    // resolver errro de array , ver como puxar as informaçoes da morte = kevin fazer
+    // $urlMorte = "https://www.breakingbadapi.com/api/deaths";
+    $urlMorte = "https://www.breakingbadapi.com/api/random-death?limit=2&offset=1";
     $dados = file_get_contents($urlMorte);
     $dados = json_decode($dados);
 
-    foreach($dados as $morte){
-        // resolver errro de array , ver como puxar as informaçoes da morte = kevin fazer
-        ?>   
-            <p  ><?= $morte ?></p>
-            <img src="<?=  $morte ?>" alt="">
-        <?php
-    }
+    // $morteRandon = array_rand($dados, 1);
+    
+    echo"$dados->death";
+    echo "<img src='$dados->img'>";
+    
+
+    
+    
 ?>
 
