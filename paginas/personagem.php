4379b8312bc4ee37a5052b9990ba832a4383d33c
marguilherme <84818691+marguilherme@users.noreply.github.com>
2022-10-19 14:04:58 -0300

    todas as api consumidas

--- paginas/personagem.php
+++ paginas/personagem.php
@@ -12,7 +12,7 @@
     $dados = json_decode($dados);
 
     ?>
-    <div class="card mb-3" style="max-width: 100%;">
+    <div class="card mb-3" style="max-width: 100%; max-height: auto;">
     <div class="row g-0">
       <div class="col-md-4">
       <img src='<?= $dados[0]->img ?>' class='img-fluid rounded-start w-100' style="height: 65vh;">        
@@ -49,7 +49,7 @@
             </h4>
             <h4 class="card-text"><small class="text-muted">Apareceu em:
             <strong><?= $dados[0]->category ?></strong> </small>
-
+            </h4>
             <h4 class="card-text text-muted" > Temporadas:
             <?php
                 foreach($dados[0]->appearance as $data){
@@ -62,6 +62,7 @@
                 <?php
             }
             ?>
+            </h4>
             <h4 class="card-text"><small class="text-muted">Frase: 
                 <?php
 
@@ -73,11 +74,30 @@
                 if( empty($dadosFrase[0]->quote)){
                     return null;
                 }
+                foreach($dadosFrase as $data){
+                    ?>
+                    <small class="card-text">
+                        <strong>
+                            <?= $data->quote ?><br>
+                        </strong> 
+                    </small>
+                <?php
+                }
                 ?>
-                <small><strong><?= $dadosFrase[0]->quote ?></strong></small>
             </h4>
-            </h4>
-          </h4>
+            <h4 class="card-text"><small class="text-muted">Mortes:
+                <?php
+                $nome = str_replace(' ', '+', $dados[0]->name);
+                $urlContador = "https://www.breakingbadapi.com/api/death-count?author={$nome}";
+                $dadosContador = file_get_contents($urlContador);
+                $dadosContador = json_decode($dadosContador);
+
+                if(empty($dadosContador)){
+                  return null;
+                }
+                ?>
+                <small><strong><?= $dadosContador[0]->deathCount ?></strong></small>
+            </small></h4>
         </div>
       </div>
     </div>
