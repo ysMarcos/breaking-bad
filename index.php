f8d0b8441d8e14460311918730e59e1bc2f47024
NivekSotam <kevin.matoss.km@gmail.com>
2022-10-19 21:31:16 -0300

    home beta

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quebrando ruim API</title>
     <!-- local host do marcos -->
-    <base href="http://localhost/breaking-bad/">
+    <!-- <base href="http://localhost/breaking-bad/"> -->
     <!-- local host do kevin -->
-    <!-- <base href="http://localhost:81/faculdade/breaking-bad/breaking-bad/">  -->
+    <base href="http://localhost:81/faculdade/breaking-bad/breaking-bad/"> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="./css/style.css">
--- paginas/home.php
+++ paginas/home.php
@@ -13,16 +13,21 @@
         foreach($dados as $personagem){
             $img = $personagem->img;
             ?>
-            <div class="col-12 col-md-3">
-                    <div class="card">
-                        <img src="<?= $img ?>" class="w-100">
-                        <div class="card-body text-center">
-                            <p><strong><?= $personagem->name ?></strong></p>
-                            <p><a href="personagem/<?= $personagem->char_id ?>" >top</a></p>
-                        </div>
-                    </div>
-                </div>
+           
+           <div class="col-12 col-md-3">
+            <div class="card">
+            <img src="<?= $img ?>" class="card-grid w-100" style="height:40vh">
+            <div class="class card-body text-center">
+            <p>
+                                <strong><?= $personagem->name ?></strong>
+                                <a href="personagem/<?= $personagem->char_id ?>" class="btn btn-warning">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/></svg>
+                                </a>
+            </p>
             </div>
+            </div>
+            </div>
+            
             <?php
         }
     ?>
